ad09c66e2bc6792849bd819334f1a7bdc5ef6e95
刘倚帆 <15277196+liu-yifan0108@user.noreply.gitee.com>
2025-04-16 23:07:27 +0800

    Initial Commit

---
+++ Pawgress/PawgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawgressApp.swift
+//  Pawgress
+//
+//  Created by Emily on 2025/4/16.
+//
+
+import SwiftUI
+
+@main
+struct PawgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawgressTests/PawgressTests.swift
@@ -0,0 +1,16 @@
+//
+//  PawgressTests.swift
+//  PawgressTests
+//
+//  Created by Emily on 2025/4/16.
+//
+
+import Testing
+
+struct PawgressTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
