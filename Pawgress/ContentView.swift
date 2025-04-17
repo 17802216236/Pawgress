2402c43fe3b115a8de36129a026285adbbc6f936
刘倚帆 <15277196+liu-yifan0108@user.noreply.gitee.com>
2025-04-17 11:46:56 +0800

    Project created

--- Pawgress/ContentView.swift
+++ Pawgress/ContentView.swift
@@ -4,18 +4,20 @@
 //
 //  Created by Emily on 2025/4/16.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Image("rabbits")
+            .resizable()
+            .cornerRadius(20.0)
+            .aspectRatio(contentMode: .fit)
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        
+        Text("Welcome to Pawgress!")
+
     }
 }
 
